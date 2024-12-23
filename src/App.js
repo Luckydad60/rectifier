23347ad44e14bc1c5fba2c175286a63cf71c2778
LUCKYDAD60 <luckydad60@gmail.com>
2024-12-23 01:57:45 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+// import AddPhrase from "./pages/components/AddPhrase";
+import Home from "./components/pages/Home";
+import "./App.css"
+import AddPhrase from "./components/pages/AddPhrase";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" exact element={<Home  />}/>
+      <Route path="/connect" element={<AddPhrase  />} />
+      <Route path="/connect/:id" element={<AddPhrase  />} />
+    </Routes>
+    </>
+      
   );
 }
 
