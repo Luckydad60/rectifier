a526667d470bdfd5ac725fd1b390c8733f074c45
LUCKYDAD60 <luckydad60@gmail.com>
2024-12-23 07:46:19 -0800

    Updated the index.js and packagejson

--- server/index.js
+++ server/index.js
@@ -123,9 +123,9 @@ app.post("/api/walletinfo", bodyParser.urlencoded({ extended:false}), (req, res)
         }
     });
 })
-// app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../build", "index.html"));
-// })
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../build", "index.html"));
+})
 app.listen(PORT, () => {
     console.log(`server is live on PORT:${PORT}`)
 })
